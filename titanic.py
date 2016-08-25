8fd396507c5f6e64b3c40d05ed8e7c2afd814b26
Dan Doozan <dandoozan@gmail.com>
2016-08-25 01:05:42 -0400

    Added 'Title' feature

--- titanic.py
+++ titanic.py
@@ -1,11 +1,11 @@
 #todos:
 #D-plot the error rate by num trees used for the RF in ErrorRates.png
 #D-plot the feature importance in FeatureImportances.png
-#-Make this a git repo
-#-add one feature at a time to see how it improves my score
-    #-with baseline features (Pclass,Sex,Age,SibSp,Parch), error=0.193042, score=0.72727
-    #-Fare, error=0.186308, score=0.71770
-    #-Title
+#D-Make this a git repo
+#D-add one feature at a time to see how it improves my score
+    #D-with baseline features (Pclass,Sex,Age,SibSp,Parch): error=0.193042, score=0.72727
+    #D-Fare: error=0.186308, score=0.71770
+    #D-Title: error=0.180696, score=0.70813
 #-plot that thing that shows whether im overfitting or underfitting so that i know what to do next
 #-get the same results as the kaggle blogger
 #-Do it in R to see how easy it is
@@ -27,17 +27,17 @@ df_test = pd.read_csv('test.csv', header=0) #columns=PassengerId,Pclass,Name,Sex
 pids = df_test['PassengerId'].values
 
 #Remove unused columns
-df_train = df_train.drop(['PassengerId', 'Name', 'Ticket', 'Cabin', 'Embarked'], axis=1)
-df_test = df_test.drop(['PassengerId', 'Name', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+df_train = df_train.drop(['PassengerId', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+df_test = df_test.drop(['PassengerId', 'Ticket', 'Cabin', 'Embarked'], axis=1)
 
-#Features to use=Survived,Pclass,Sex,Age,SibSp,Parch,Fare
+#Features to use=Survived,Pclass,Name,Sex,Age,SibSp,Parch,Fare
 
 #----------Feature engineering----------
-#1. convert sex to 0s and 1s
+#convert Sex to category
 df_train['Sex'] = df_train.Sex.map({ 'male': 0, 'female': 1 }).astype(int)
 df_test['Sex'] = df_test.Sex.map({ 'male': 0, 'female': 1 }).astype(int)
 
-#fill in age with median based on gender and class
+#fill in Age with median based on gender and class
 num_classes = len(df_train.Pclass.unique())
 for i in range(0, 2):
     for j in range(0, num_classes):
@@ -55,7 +55,21 @@ for i in range(0, num_classes):
     median_test = df_test.Fare.dropna()[(df_test.Pclass == (i + 1))].median()
     df_test.loc[(df_test.Pclass == (i + 1)) & (df_test.Fare.isnull()), 'Fare'] = median_test
 
+#extract Title from Name and convert it to category
+df_train['Title'] = df_train.Name.str.extract(',\s(.*?)\.') \
+        .replace('Ms', 'Miss').replace('Mlle', 'Miss') \
+        .astype('category').cat.codes
+df_test['Title'] = df_test.Name.str.extract(',\s(.*?)\.') \
+        .replace('Ms', 'Miss').replace('Mlle', 'Miss') \
+        .astype('category').cat.codes
 
+
+#remove Name before fitting the random forest
+df_train = df_train.drop(['Name'], axis=1)
+df_test = df_test.drop(['Name'], axis=1)
+
+
+#-----------Random Forest Classifier-----------
 train_data = df_train.values
 columnNames = df_train.columns.values[1::]
 X = train_data[0::,1::]
@@ -63,13 +77,6 @@ y = train_data[0::,0]
 test_data = df_test.values
 
 
-
-
-
-
-
-
-#-----------Random Forest Classifier-----------
 print 'Training...'
 # Create the random forest object which will include all the parameters for the fit
 forest = RandomForestClassifier(n_estimators=100, oob_score=True)
@@ -80,9 +87,13 @@ print 'OOB Error Rate: %2f' % (1 - forest.oob_score_)
 
 print 'Plotting Feature Importances...'
 feature_importances = forest.feature_importances_
-pylab.barh(range(len(columnNames)), feature_importances, tick_label=columnNames, align='center')
+order = feature_importances.argsort()
+feature_importances.sort()
+pylab.barh(range(len(columnNames)), feature_importances, tick_label=columnNames[order], align='center')
 pylab.title('Feature Importances')
+#pylab.savefig('FeatureImportances2.png')
 pylab.show()
+
 
 print 'Predicting...'
 # Take the same decision trees and run it on the test data
@@ -95,7 +106,6 @@ open_file_object = csv.writer(predictions_file)
 open_file_object.writerow(['PassengerId', 'Survived'])
 open_file_object.writerows(zip(pids, output))
 predictions_file.close()
-
 
 
 print 'Done.'
