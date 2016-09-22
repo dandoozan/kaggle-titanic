a4166e65837babd3c42116a261216d2266b6099a
Dan Doozan <dandoozan@gmail.com>
2016-09-22 14:11:07 -0400

    [xgb] Used xgb.cv and tuned hyperparameters

--- titanic_xgb.R
+++ titanic_xgb.R
@@ -1,21 +1,39 @@
 #todo:
-#D-do simple xgboost: r_xgb: final train-error=0.017957, score=0.72727
-#-use xgboost cv feature
-#-plot train-error
-#-plot feature importances
+#D-do simple xgboost: r_xgb: finalTrainError=0.017957, finalTestError=?, score=0.72727
+#D-use xgb.cv to tune hyperparameters: r_xgb_hyperparam: 0.166388, 0.172898, 0.78947
+#-plot feature importances (read: https://cran.r-project.org/web/packages/xgboost/vignettes/discoverYourData.html)
 #-plot learning curve
-#-do more complex xgboost
+#-do more complex xgboost (read: http://xgboost.readthedocs.io/en/latest/R-package/xgboostPresentation.html)
 #-perhaps adjust the threshold for 1 vs 0 (from 0.5 to 0.7 or something?)
-
+#-maybe implement early stopping
 
 #Remove all objects from the current workspace
 rm(list = ls())
 
 library(xgboost)
+library(Matrix) #sparse.model.matrix
 
 #Globals
-FILENAME = 'r_xgb'
+FILENAME = 'r_xgb_hyperparam'
 PROD_RUN = T
+
+plotCV = function(data, label, params, nrounds, verbose) {
+  print('Plotting Error Rates...')
+  set.seed(754)
+  cvRes <- xgb.cv(data=data,
+                  label=label,
+                  params=params,
+                  nfold=5,
+                  nrounds=nrounds,
+                  verbose=verbose)
+  print(paste('Final Train Error:', cvRes$train.error.mean[length(cvRes$train.error.mean)]))
+  print(paste('Final Test Error:', cvRes$test.error.mean[length(cvRes$test.error.mean)]))
+
+  #plot train error vs test error
+  plot(cvRes$train.error.mean, type='l', ylim = c(min(cvRes$train.error.mean, cvRes$test.error.mean), max(cvRes$train.error.mean, cvRes$test.error.mean)), col='blue', main='Train Error vs. Test Error', xlab='Num Rounds', ylab='Error')
+  lines(cvRes$test.error.mean, col='red')
+  legend(x='topright', legend=c('train', 'test'), fill=c('blue', 'red'), inset=0.02, text.width=15)
+}
 
 #============= Main ================
 
@@ -27,17 +45,40 @@ set.seed(634)
 trainSparseMatrix = sparse.model.matrix(Survived~.-1, data=subset(train, select=-c(PassengerId, Name, Ticket, Cabin)))
 testSparseMatrix = sparse.model.matrix(~.-1, data=subset(test, select=-c(PassengerId, Name, Ticket, Cabin)))
 
+
+#set hyper params
+nrounds = 100
+verbose = 0
+xgbParams <- list(
+    'objective'='binary:logistic',
+
+    #range=[0,1], default=0.3, toTry=0.01,0.015,0.025,0.05,0.1
+    'eta'=0.001, #learning rate. Lower value=less overfitting
+
+    #range=[1,∞], default=6, toTry=3,5,7,9,12,15,17,25
+    'max_depth'=3, #Lower value=less overfitting
+
+    #range=[0,∞], default=1, toTry=1,3,5,7
+    'min_child_weight'=1,  #Larger value=less overfitting
+
+    #range=(0,1], default=1, toTry=0.6,0.7,0.8,0.9,1.0
+    'subsample'=1, #ratio of sample of data to use for each instance (eg. 0.5=50% of data). Lower value=less overfitting
+
+    #range=(0,1], default=1, toTry=0.6,0.7,0.8,0.9,1.0
+    'colsample_bytree'=0.6 #ratio of cols (features) to use in each tree
+  )
+
+#plot cv
+plotCV(trainSparseMatrix, train$Survived, xgbParams, nrounds, verbose)
+
 #create model
 print('Creating Model...')
 set.seed(754)
 model = xgboost(data=trainSparseMatrix,
-                 label=train$Survived,
-                 max.depth=4,
-                 eta=1,
-                 nthread=2,
-                 nround=100,
-                 verbose=0,
-                 objective='binary:logistic')
+                  label=train$Survived,
+                  params=xgbParams,
+                  nrounds=nrounds,
+                  verbose=verbose)
 
 if (PROD_RUN) {
   #Output solution
